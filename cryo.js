933cbb73cc6b0c09b0c9d6f663a8c8fe5890d2eb
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-03-13 12:10:00 +0100

    Prepare all commands the xCryo module

--- cryo.js
+++ cryo.js
@@ -1,17 +1,24 @@
 'use strict';
 
-const cmd = {};
-
-cmd['do'] = function*(msg, resp) {
-  const cryo = require('.');
-
-  try {
-    yield cryo.do(resp);
-    resp.events.send(`cryo.do.${msg.id}.finished`);
-  } catch (ex) {
-    resp.events.send(`cryo.do.${msg.id}.error`, ex);
-  }
+const cmd = {
+  create: null,
+  sync: null,
+  freeze: null,
+  thaw: null,
 };
+
+Object.keys(cmd).forEach(c => {
+  cmd[c] = function*(msg, resp) {
+    const cryo = require('.');
+
+    try {
+      yield cryo[c](resp, msg);
+      resp.events.send(`cryo.${c}.${msg.id}.finished`);
+    } catch (ex) {
+      resp.events.send(`cryo.${c}.${msg.id}.error`, ex);
+    }
+  };
+});
 
 /**
  * Retrieve the list of available commands.
@@ -22,9 +29,33 @@ exports.xcraftCommands = function() {
   return {
     handlers: cmd,
     rc: {
-      do: {
+      create: {
         parallel: true,
-        desc: '//do some...',
+        desc: 'create a new action table',
+        options: {
+          params: {
+            required: 'table',
+            optional: 'options',
+          },
+        },
+      },
+    },
+    sync: {
+      parallel: true,
+      desc: 'sync the store to the disk',
+    },
+    freeze: {
+      parallel: true,
+      desc: 'freeze (persist) an action in the store',
+      options: {
+        required: ['action', 'rules'],
+      },
+    },
+    thaw: {
+      parallel: true,
+      desc: 'thaw (extract) the actions from the store',
+      options: {
+        required: ['table'],
       },
     },
   };
