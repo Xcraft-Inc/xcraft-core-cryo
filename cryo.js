585c5ae4a88f627a999fa88a6ce04c786bfeb709
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-04-17 17:48:09 +0200

    Add a new cryo.branch command for creating a new branch

--- cryo.js
+++ cryo.js
@@ -4,17 +4,35 @@ const cryo = require('.');
 
 const cmd = {};
 
+function isFunction(fn) {
+  return typeof fn === 'function';
+}
+
+function isGenerator(fn) {
+  return (
+    fn &&
+    isFunction(fn) &&
+    fn.constructor &&
+    fn.constructor.name === 'GeneratorFunction'
+  );
+}
+
 const proto = Object.getPrototypeOf(cryo);
 Object.getOwnPropertyNames(proto)
   .filter(name => name !== 'constructor' && !name.startsWith('_'))
   .filter(name => {
     const desc = Object.getOwnPropertyDescriptor(proto, name);
-    return !!desc && typeof desc.value === 'function';
+    return !!desc && isFunction(desc.value);
   })
   .forEach(name => {
-    cmd[name] = function(msg, resp) {
+    cmd[name] = function*(msg, resp) {
       try {
-        const results = cryo[name](resp, msg);
+        let results;
+        if (isGenerator(cryo[name])) {
+          results = yield cryo[name](resp, msg);
+        } else {
+          results = cryo[name](resp, msg);
+        }
         resp.events.send(`cryo.${name}.${msg.id}.finished`, results);
       } catch (ex) {
         resp.events.send(`cryo.${name}.${msg.id}.error`, ex);
@@ -34,6 +52,13 @@ exports.xcraftCommands = function() {
       close: {
         parallel: true,
         desc: 'close the table',
+      },
+      branch: {
+        parallel: true,
+        desc: 'create a new branch',
+        options: {
+          required: 'db',
+        },
       },
       freeze: {
         parallel: true,
