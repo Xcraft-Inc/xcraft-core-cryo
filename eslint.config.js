1a214ef39b8230fd3081686559eaec9b826be3ff
Gillian Fries <fries@epsitec.ch>
2024-09-03 11:58:42 +0200

    Fix merge conflict

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,3 @@
-<<<<<<< Updated upstream
-'use strict';
-
-module.exports = {
-  root: true,
-  parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  env: {
-    browser: true,
-    es2022: true,
-    mocha: true,
-    node: true,
-  },
-  plugins: ['react', 'babel', 'jsdoc'],
-  extends: [
-    'prettier',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:jsdoc/recommended',
-  ],
-  rules: {
-    // Other rules
-    'no-console': 'off',
-    'eqeqeq': 'error',
-    'react/display-name': 'off',
-  },
-};
-=======
 const js = require('@eslint/js');
 const globals = require('globals');
 const react = require('eslint-plugin-react');
@@ -92,4 +60,3 @@ module.exports = [
     },
   },
 ];
->>>>>>> Stashed changes
